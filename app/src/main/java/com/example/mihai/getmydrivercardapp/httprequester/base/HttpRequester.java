38bf3d4998dd3e7eeb404a32c6bcb1329e6c400b
Mihail-Iliev13 <mihaililiev13@gmail.com>
2018-10-23 11:32:28 +0300

    added json parser and http requester

---
+++ app/src/main/java/com/example/mihai/getmydrivercardapp/httprequester/base/HttpRequester.java
@@ -0,0 +1,10 @@
+package com.example.mihai.getmydrivercardapp.httprequester.base;
+
+import java.io.IOException;
+
+public interface HttpRequester {
+    String get(String url) throws IOException;
+
+    String post(String url, String body) throws IOException;
+}
+
