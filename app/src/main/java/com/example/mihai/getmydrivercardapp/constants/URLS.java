0b440e19581fa3caf4bca2ed31638b4696903d18
Mihail-Iliev13 <mihaililiev13@gmail.com>
2018-11-11 22:06:22 +0200

    fix some bugs

--- app/src/main/java/com/example/mihai/getmydrivercardapp/constants/URLS.java
+++ app/src/main/java/com/example/mihai/getmydrivercardapp/constants/URLS.java
@@ -1,5 +1,5 @@
 package com.example.mihai.getmydrivercardapp.constants;
 
 public class URLS {
-        public static final String BASE_SERVER_URL = "http://192.168.43.191:8080";
+        public static final String BASE_SERVER_URL = "http://192.168.1.103:8080";
 }
